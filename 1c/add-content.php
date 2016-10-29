e746cc8115ebb8d38cb95dce0333e3f42b20956f
vivz <vivianzhang61@gmail.com>
2016-10-29 02:02:43 -0700

    search box

--- 1c/add-content.php
+++ 1c/add-content.php
@@ -159,15 +159,66 @@
                     <div class="col-md-6">
                         <div class="panel panel-default">
                         <div class="panel-heading">
-                            Animated Progress Bars 
+                           BASIC  FORM ELEMENTS
                         </div>
-                       
                         <div class="panel-body">
-                       <div class="progress progress-striped active">
-  <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-    <span class="sr-only">40% Complete (success)</span>
+                       <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" />
   </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+  </div>
+  <div class="form-group">
+    <label for="exampleInputFile">File input</label>
+    <input type="file" id="exampleInputFile" />
+    <p class="help-block">Example block-level help text here.</p>
+  </div>
+  <div class="checkbox">
+    <label>
+      <input type="checkbox" /> Check me out
+    </label>
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+                           <hr />
+                           <input type="text" class="form-control" placeholder="Text input" />
+                           <hr />
+                           <textarea class="form-control" rows="3" placeholder="Text Area"></textarea>
+                           <hr />
+                           <div class="checkbox">
+  <label>
+    <input type="checkbox" value="" />
+    Option one is this and that&mdash;be sure to include why it's great
+  </label>
 </div>
+<div class="checkbox disabled">
+  <label>
+    <input type="checkbox" value="" disabled />
+    Option two is disabled
+  </label>
+</div>
+
+<div class="radio">
+  <label>
+    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked />
+    Option one is this and that&mdash;be sure to include why it's great
+  </label>
+</div>
+<div class="radio">
+  <label>
+    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2" />
+    Option two can be something else and selecting it will deselect option one
+  </label>
+</div>
+<div class="radio disabled">
+  <label>
+    <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3" disabled />
+    Option three is disabled
+  </label>
+</div>
+</form>
                             </div>
                             </div>
                     </div>
