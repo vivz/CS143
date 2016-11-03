73e0547509f9aab5d0a636de4e7153e153c58226
vivz <vivianzhang61@gmail.com>
2016-11-03 01:23:12 -0700

    front end for the browsing page

--- 1c/index.php
+++ 1c/index.php
@@ -60,7 +60,7 @@
                         <ul id="menu-top" class="nav navbar-nav navbar-right">
                             <li><a class="menu-top-active" href="index.php">Welcome</a></li>
                             <li><a href="add-content.php">Add Content</a></li>
-                            <li><a href="browse.php">Browse</a></li>
+                            <li><a href="show-movie.php">Browse</a></li>
                        <!--     <li><a href="forms.html">Search</a></li>
                             <li><a href="blank.html">Blank Page</a></li>-->
 
